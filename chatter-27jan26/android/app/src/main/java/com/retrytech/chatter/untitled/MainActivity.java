14b198b141ecb364702d387fd4ca37409d020fa8
darkguy123 <12982248+darkguy123@users.noreply.github.com>
2026-06-27 14:40:01 +0100

    pull

---
+++ chatter-27jan26/android/app/src/main/java/com/retrytech/chatter/untitled/MainActivity.java
@@ -0,0 +1,7 @@
+package com.retrytech.chatter.untitled;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    // GeneratedPluginRegistrant automatically registers native bindings.
+}
--- chatter-27jan26/android/app/src/main/kotlin/com/retrytech/chatter/untitled/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.retrytech.chatter.untitled
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-    // The GeneratedPluginRegistrant automatically handles plugin registration on modern Flutter.
-}

8c482bbc97b8d9e0beb3c4ca6aa8c3baf367f486
darkguy123 <12982248+darkguy123@users.noreply.github.com>
2026-06-29 10:45:54 +0100

    Add Android biometric permission and extend FlutterFragmentActivity in MainActivity to support local_auth

--- chatter-27jan26/android/app/src/main/java/com/lumosocial/app/MainActivity.java
+++ chatter-27jan26/android/app/src/main/java/com/lumosocial/app/MainActivity.java
@@ -1,7 +1,7 @@
 package com.lumosocial.app;
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterFragmentActivity {
     // GeneratedPluginRegistrant automatically registers native bindings.
 }
